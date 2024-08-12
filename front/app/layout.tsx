9469493629cba72c66b17153c217aab18fd490f6
Victor Rodrigues <victorvieira89@gmail.com>
2024-08-12 15:01:28 -0300

    feat: add radix themes

--- front/app/layout.tsx
+++ front/app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Theme } from "@radix-ui/themes";
+
+import "@radix-ui/themes/styles.css";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,7 +19,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Theme accentColor="orange" radius="small">
+          {children}
+        </Theme>
+      </body>
     </html>
   );
 }
